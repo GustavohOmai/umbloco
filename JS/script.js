f3e04bea25388ff47629307b4820eb935417174f
GustavoOmai <omai.gustavoh@gmail.com>
2023-02-09 19:07:41 -0300

    Desktop Version

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,39 @@
-Script
+const nav = document.querySelector("header");
+const scene = document.getElementById('scene');
+
+
+scene.addEventListener("mouseover", () => {
+    const parallaxInstance = new Parallax(scene);
+})
+
+
+
+window.onscroll = function () {
+    scroll();
+}
+
+function scroll() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        ;
+        nav.classList.add("header-on");
+        
+    } else {
+        nav.classList.remove("header-on");
+       
+    }
+}
+
+
+
+
+$(document).ready(function () {
+
+    $('.banner-slick').slick({
+        dots: true,
+        infinite: true,
+        arrows:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 300,
+    });
+});
